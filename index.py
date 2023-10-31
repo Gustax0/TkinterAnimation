ccae922c776d848cf70936e9c6dda0545c659fac
Gustax0 <gustavohenriquegomesdias@gmail.com>
2023-10-31 17:49:57 -0300

    Ta bugando

--- index.py
+++ index.py
@@ -8,34 +8,34 @@ root = tk.Tk()
 class App:
     def __init__(self):
         self.Interface()
-        self.idle = Image.open("assets/mario_idle.png")  # move the initialization here
         self.Botao()
         self.Imagens()
 
     def Interface(self):
-        self.screen_center_x = root.winfo_screenwidth() // 2
-        self.screen_center_y = root.winfo_screenheight() // 2
-        root.geometry(f"800x600+{self.screen_center_x - 400}+{self.screen_center_y - 300}")
+        self.screen_width = root.winfo_screenwidth()
+        self.screen_height = root.winfo_screenheight()
+        root.geometry(f"800x600+{self.screen_width // 2 - 400}+{self.screen_height // 2 - 300}")
         root.resizable(True, True)
         root.title("Animando")
 
     def Botao(self):
-        button_center_x = self.screen_center_x / 1.5
-        button_center_y = self.screen_center_y / 1.5 + 200
+        button_center_x = self.screen_width // 2
+        button_center_y = self.screen_height // 2 + 200
         botao = Button(
             root, text="Animar", bd=0, fg="#f54245", bg="#f27e80", command=self.Animar
         )
         botao.place(x=button_center_x, y=button_center_y)
 
     def Imagens(self):
-        sprite_center_x = self.screen_center_x - self.idle.width / 2
-        sprite_center_y = self.screen_center_y - self.idle.height / 2
+        self.idle = Image.open("assets/mario_idle.png")
+        width, height = self.idle.size
+        factor = min(self.screen_width / width, self.screen_height / height)
+        new_width = int(width * factor)
+        new_height = int(height * factor)
+        self.idle = self.idle.resize((new_width, new_height), Image.ANTIALIAS)
         self.idle_tk = ImageTk.PhotoImage(self.idle)
-        self.idle_frame = tk.Frame(root)
-        self.idle_frame.place(x=sprite_center_x, y=sprite_center_y)
-        self.idle_label = tk.Label(self.idle_frame, image=self.idle_tk)
-        self.idle_label.pack(fill=tk.BOTH, expand=tk.NO)
-        self.idle_label.image = self.idle_tk
+        self.idle_label = tk.Label(root, image=self.idle_tk)
+        self.idle_label.place(x=(self.screen_width - new_width) // 2, y=(self.screen_height - new_height) // 2)
 
     def Animar(self):
         self.Pular()
@@ -44,12 +44,22 @@ class App:
 
     def Pular(self):
         jumping = Image.open("assets/mario_jumping.png")
+        width, height = jumping.size
+        factor = min(self.screen_width / width, self.screen_height / height)
+        new_width = int(width * factor)
+        new_height = int(height * factor)
+        jumping = jumping.resize((new_width, new_height), Image.ANTIALIAS)
         jumping_tk = ImageTk.PhotoImage(jumping)
         self.idle_label.config(image=jumping_tk)
         self.idle_label.image = jumping_tk
 
     def Cair(self):
         falling = Image.open("assets/mario_falling.png")
+        width, height = falling.size
+        factor = min(self.screen_width / width, self.screen_height / height)
+        new_width = int(width * factor)
+        new_height = int(height * factor)
+        falling = falling.resize((new_width, new_height), Image.ANTIALIAS)
         falling_tk = ImageTk.PhotoImage(falling)
         self.idle_label.config(image=falling_tk)
         self.idle_label.image = falling_tk
