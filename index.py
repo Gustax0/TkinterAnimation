724468e15d356606323f058b0f867221a16fdfae
Gustax0 <gustavohenriquegomesdias@gmail.com>
2023-10-31 17:39:29 -0300

    Update index.py

--- index.py
+++ index.py
@@ -5,13 +5,10 @@ import time
 
 root = tk.Tk()
 
-
 class App:
     def __init__(self):
         self.Interface()
         self.Botao()
-        self.Pular
-        self.Cair()
         self.Imagens()
 
     def Interface(self):
@@ -23,37 +20,35 @@ class App:
 
     def Botao(self):
         botao = Button(
-            root, text="Animar", bd=0, fg="#f54245", bg="#f27e80", command=self.Pular
+            root, text="Animar", bd=0, fg="#f54245", bg="#f27e80", command=self.Animar
         )
         botao.place(x=640, y=360)
 
     def Imagens(self):
-        idle = Image.open("assets/mario_idle.png")
-        idle_tk = ImageTk.PhotoImage(idle)
-        idle_frame = tk.Frame(root)
-        idle_frame.place(x=510, y=100)
-        idle_label = tk.Label(idle_frame, image=idle_tk)
-        idle_label.pack(fill=tk.BOTH, expand=tk.NO)
-        idle_label.image = idle_tk
+        self.idle = Image.open("assets/mario_idle.png")
+        self.idle_tk = ImageTk.PhotoImage(self.idle)
+        self.idle_frame = tk.Frame(root)
+        self.idle_frame.place(x=510, y=100)
+        self.idle_label = tk.Label(self.idle_frame, image=self.idle_tk)
+        self.idle_label.pack(fill=tk.BOTH, expand=tk.NO)
+        self.idle_label.image = self.idle_tk
+
+    def Animar(self):
+        self.Pular()
+        time.sleep(1)
+        self.Cair()
 
     def Pular(self):
-        idle = Image.open("assets/mario_jumping.png")
-        idle_tk = ImageTk.PhotoImage(idle)
-        idle_frame = tk.Frame(root)
-        idle_frame.place(x=510, y=70)
-        idle_label = tk.Label(idle_frame, image=idle_tk)
-        idle_label.pack(fill=tk.BOTH, expand=tk.NO)
-        idle_label.image = idle_tk
+        jumping = Image.open("assets/mario_jumping.png")
+        jumping_tk = ImageTk.PhotoImage(jumping)
+        self.idle_label.config(image=jumping_tk)
+        self.idle_label.image = jumping_tk
 
     def Cair(self):
-        idle = Image.open("assets/mario_falling.png")
-        idle_tk = ImageTk.PhotoImage(idle)
-        idle_frame = tk.Frame(root)
-        idle_frame.place(x=510, y=100)
-        idle_label = tk.Label(idle_frame, image=idle_tk)
-        idle_label.pack(fill=tk.BOTH, expand=tk.NO)
-        idle_label.image = idle_tk
-
+        falling = Image.open("assets/mario_falling.png")
+        falling_tk = ImageTk.PhotoImage(falling)
+        self.idle_label.config(image=falling_tk)
+        self.idle_label.image = falling_tk
 
 if __name__ == "__main__":
     app = App()
